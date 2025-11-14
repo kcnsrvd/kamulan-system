8f9043a8a9557d3f88e59c541ff06fb888cb03e8
kyla <152973548+kcnsrvd@users.noreply.github.com>
2025-11-14 20:38:42 +0800

    Delete kamulan-system directory

--- kamulan-system/auth/logout.php
+++
@@ -1,6 +0,0 @@
-<?php
-require_once(__DIR__ . '/../config/session.php');
-session_destroy();
-header('Location: /kamulan-system/index.php');
-exit;
-?>
--- kamulan-system/auth/verify_session.php
+++
@@ -1,4 +0,0 @@
-<?php
-require_once(__DIR__ . '/../config/session.php');
-if (isset($_SESSION['user'])) echo json_encode(['logged'=>true,'user'=>$_SESSION['user']]); else echo json_encode(['logged'=>false]);
-?>
